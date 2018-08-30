ee453da75b1edb403b6de725f275de39e15a6ae0
Derek Kwan <derek.x.kwan@gmail.com>
2018-08-30 16:22:46 -0700

    Update top_to_csv.sh

--- top_to_csv.sh
+++ top_to_csv.sh
@@ -1,4 +1,5 @@
 # pipes top info to csv
 #for sed, the 7 includes header, use 8 to remove
+# -n controls how many times to update
 
-top -b -n 30 -o P | sed -n '7, 50{s/^ *//;s/ *$//;s/  */,/gp;};30q'
+top -b -n 1 -o P | sed -n '7, 50{s/^ *//;s/ *$//;s/  */,/gp;};30q'
